c7ce7ce9e93440acdc3e472215f700d9ab431c9b
adelarja <adelarja@gmail.com>
2023-04-29 02:26:09 -0300

    Changed comments

--- GiftList/client/index.js
+++ GiftList/client/index.js
@@ -5,16 +5,16 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list?
-
+  // TODO: In the future, I'll try to improve this part of the project creating a frontend.
   let name = "Adel Arja";
   let merkleTree = new MerkleTree(niceList);
   let index = niceList.findIndex(n => n === name);
   let proof = merkleTree.getProof(index);
 
+  // As a client, we'll send the value (our name) and the proof so the server can verify that we are part of the list.
+  // If we provide a name that is in the list, we receive the message "You got a toy robot!".
+  // If we provide a name that is not in the list, we receive the message "You are not on the list :(".
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-    // As a client, we'll send the value (our name) and the proof so the server can verify that we are part of the list.
     proof: proof,
     name: name
   });
--- GiftList/server/index.js
+++ GiftList/server/index.js
@@ -9,7 +9,8 @@ app.use(express.json());
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
 
-// We know the nicelist root. So what we do is to 
+// We calculated the nicelist root using the MerkleTree methods (running the example script).
+// The value is harcoded in the next line.
 const MERKLE_ROOT = '02deab0e2355c295cf78e5486796b6bb97f4d7c1d1aed363a76181f3799b55e5';
 
 app.post('/gift', (req, res) => {
@@ -18,8 +19,6 @@ app.post('/gift', (req, res) => {
 
   let isInTheList = verifyProof(body['proof'], body['name'], MERKLE_ROOT);
 
-  // TODO: prove that a name is in the list 
-  // const isInTheList = false;
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
