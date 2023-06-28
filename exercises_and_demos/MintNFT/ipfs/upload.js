f36bee1577aedbc199e7b14955b03f143d375903
adelarja <adelarja@gmail.com>
2023-06-28 00:21:08 -0300

    More demo apps

---
+++ exercises_and_demos/MintNFT/ipfs/upload.js
@@ -0,0 +1,36 @@
+async function run() {
+    const { create } = await import('ipfs-http-client');
+    const ipfs = await create();
+    
+    // we added three attributes, add as many as you want!
+    const metadata = {
+        path: '/',
+        content: JSON.stringify({
+            name: "Funny Pickachu",
+            attributes: [
+            {
+                "trait_type": "Peace",
+                "value": "10" 
+            },
+            {
+                "trait_type": "Love",
+                "value": "100"
+            },
+            {
+                "trait_type": "Web3",
+                "value": "1000"
+            }
+            ],
+            // update the IPFS CID to be your image CID
+            image: "https://ipfs.io/ipfs/QmRMnbPWLmABFEzFz7tEacPKtPm9pXo99bm6vaQccJWKcA",
+            description: "PikaPika!"
+        })
+    };
+
+    const result = await ipfs.add(metadata);
+    console.log(result);
+
+    process.exit(0);
+}
+
+run();
